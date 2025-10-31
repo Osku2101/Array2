0e58e729e0159f58ee5f40071bfbdabb21b49929
Oskari Salo <osku2101@gmail.com>
2025-10-31 11:11:11 +0200

    Update App.java

--- ArrayGit/ArrayGit2/src/App.java
+++ ArrayGit/ArrayGit2/src/App.java
@@ -4,3 +4,10 @@ public class App {
         String[] huonekalut;
 
          int[] lampotilat;
+
+         huonekalut = new String[4];
+
+    huonekalut[0] = "tuoli";
+    huonekalut[1] = "sohva";
+    huonekalut[2] = "pöytä";
+    huonekalut[3] = "kaappi";
